6d99a6872a9f7bb31e1a5b5003cee65e02b17a94
Steven Visser <55793407+StevenVisser@users.noreply.github.com>
2021-11-11 17:33:23 +0200

    Update and rename main.java to Data Preparation/CSVPrep.java

--- Data Preparation/CSVPrep.java
+++ Data Preparation/CSVPrep.java
@@ -7,17 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author steve
- */
-public class main
+public class CSVPrep
 {
     public static void main(String[] args)
     {
